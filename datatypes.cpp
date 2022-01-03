9ad2afc0a34661e51f52d2e15026de55656ebfa6
Swapnil <61934504+swapnil282K@users.noreply.github.com>
2022-01-03 06:05:06 +0000

    datatypes

---
+++ datatypes.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a=1;
+  cout<<a<<endl;
+  cout<<sizeof(a)<<endl;
+  
+  char b='v';
+  cout<<b<<endl;
+  cout<<sizeof(b)<<endl;
+
+  bool b1=false;
+  cout<<b1<<endl;
+  cout<<sizeof(b1)<<endl;
+
+  float f=1.23;
+  cout<<f<<endl;
+  cout<<sizeof(f)<<endl;
+
+  double d=13.23;
+  cout<<d<<endl;
+  cout<<sizeof(d)<<endl;
+}
