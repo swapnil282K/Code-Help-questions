4b0160a48650d619ce192a5720661ea4710ed7c6
Swapnil <61934504+swapnil282K@users.noreply.github.com>
2022-01-14 06:43:47 +0000

    Lecture16

---
+++ Lecture16/bubblesot.cpp
@@ -0,0 +1,36 @@
+void bubbleSort(vector<int>& arr, int n)
+{   
+    // Write your code here.
+    for(int i=1;i<n;i++)
+    {
+        //for round 1 to n-1;
+        for(int j=0;j<n-i;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+
+//Optimised code
+void bubbleSort(vector<int>& arr, int n)
+{   
+    // Write your code here.
+    for(int i=1;i<n;i++)
+    {
+        bool swapped=false;
+        //for round 1 to n-1;
+        for(int j=0;j<n-i;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+                swapped=true;
+            }
+        }
+        if(swapped==false)
+            break;
+    }
+}
